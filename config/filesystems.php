8e866c4f298dfef0dc9429530d449b62f1c4f604
renaldyrs <renaldyrizky83@gmail.com>
2025-05-06 01:38:24 +0700

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,7 +49,7 @@ return [
     'key' => env('AWS_ACCESS_KEY_ID'),
     'secret' => env('AWS_SECRET_ACCESS_KEY'),
     'region' => env('AWS_DEFAULT_REGION', 'ap-southeast-1'),
-    'bucket' => env('AWS_BUCKET'),
+    'bucket' => env('AWS_BUCKET', 'arneta'),
     'url' => env('AWS_URL'), // URL CloudFront atau laravel.cloud kamu
     'endpoint' => env('AWS_ENDPOINT'),
     'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
